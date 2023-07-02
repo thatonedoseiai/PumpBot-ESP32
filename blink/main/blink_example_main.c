9436fc4a25043e7fc6d899957342d11b5c163216
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-07-01 21:54:06 -0700

    change to SPI bus instead of I2C

--- blink/main/blink_example_main.c
+++ blink/main/blink_example_main.c
@@ -36,21 +36,38 @@ const esp_lcd_panel_dev_config_t panel_config = {
     .rgb_endian = LCD_RGB_ENDIAN_BGR,
     .bits_per_pixel = 16,
 };
-static i2c_config_t i2c_conf = {
-    .mode = I2C_MODE_MASTER,
-    .sda_io_num = EXAMPLE_PIN_NUM_SDA,
-    .scl_io_num = EXAMPLE_PIN_NUM_SCL,
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .master.clk_speed = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+static spi_bus_config_t buscfg = {
+    .sclk_io_num = EXAMPLE_PIN_NUM_SCLK,
+    .mosi_io_num = EXAMPLE_PIN_NUM_MOSI,
+    .miso_io_num = EXAMPLE_PIN_NUM_MISO,
+    .quadwp_io_num = -1, // Quad SPI LCD driver is not yet supported
+    .quadhd_io_num = -1, // Quad SPI LCD driver is not yet supported
+    .max_transfer_sz = EXAMPLE_LCD_H_RES * 80 * sizeof(uint16_t), // transfer 80 lines of pixels (assume pixel is RGB565) at most in one SPI transaction
 };
-esp_lcd_panel_io_i2c_config_t io_config = {
-    .dev_addr = EXAMPLE_I2C_HW_ADDR,
-    .control_phase_bytes = 1, // refer to LCD spec
-    .dc_bit_offset = 6,       // refer to LCD spec
+esp_lcd_panel_io_spi_config_t io_config = {
+    .dc_gpio_num = 16,
+    .cs_gpio_num = 18,
+    .pclk_hz = 6350000,
     .lcd_cmd_bits = 8,
     .lcd_param_bits = 8,
+    .spi_mode = 3,
+    .trans_queue_depth = 10,
 };
+/* static i2c_config_t i2c_conf = { */
+/*     .mode = I2C_MODE_MASTER, */
+/*     .sda_io_num = EXAMPLE_PIN_NUM_SDA, */
+/*     .scl_io_num = EXAMPLE_PIN_NUM_SCL, */
+/*     .sda_pullup_en = GPIO_PULLUP_ENABLE, */
+/*     .scl_pullup_en = GPIO_PULLUP_ENABLE, */
+/*     .master.clk_speed = EXAMPLE_LCD_PIXEL_CLOCK_HZ, */
+/* }; */
+/* esp_lcd_panel_io_i2c_config_t io_config = { */
+/*     .dev_addr = EXAMPLE_I2C_HW_ADDR, */
+/*     .control_phase_bytes = 1, // refer to LCD spec */
+/*     .dc_bit_offset = 6,       // refer to LCD spec */
+/*     .lcd_cmd_bits = 8, */
+/*     .lcd_param_bits = 8, */
+/* }; */
 
 static void blink_led(void)
 {
@@ -101,9 +118,11 @@ static void configure_led(void)
 #endif
 
 void start_lcd() {
-    ESP_ERROR_CHECK(i2c_param_config(I2C_HOST, &i2c_conf));
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_HOST, I2C_MODE_MASTER, 0, 0, 0));
-ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)I2C_HOST, &io_config, &lcd_io_handle));
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO)); // Enable the DMA feature
+                                                                        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &lcd_io_handle));
+    /* ESP_ERROR_CHECK(i2c_param_config(I2C_HOST, &i2c_conf)); */
+    /* ESP_ERROR_CHECK(i2c_driver_install(I2C_HOST, I2C_MODE_MASTER, 0, 0, 0)); */
+    /* ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)I2C_HOST, &io_config, &lcd_io_handle)); */
     ESP_ERROR_CHECK(esp_lcd_new_panel_ili9341(lcd_io_handle, &panel_config, &lcd));
 }
 
