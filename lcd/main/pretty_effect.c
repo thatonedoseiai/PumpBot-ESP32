3ab8fd1907c1f3d3601c1ca89ef6fbbaa5e54c3c
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-07-03 00:19:33 -0700

    fix some formatting

--- lcd/main/pretty_effect.c
+++ lcd/main/pretty_effect.c
@@ -17,8 +17,7 @@
 uint16_t *pixels;
 
 //Grab a rgb16 pixel from the esp32_tiles image
-static inline uint16_t get_bgnd_pixel(int x, int y)
-{
+static inline uint16_t get_bgnd_pixel(int x, int y) {
     //Get color of the pixel on x,y coords
     return (uint16_t) *(pixels + (y * IMAGE_W) + x);
 }
@@ -34,8 +33,7 @@ static int8_t xcomp[320], ycomp[240];
 //Calculate the pixel data for a set of lines (with implied line size of 320). Pixels go in dest, line is the Y-coordinate of the
 //first line to be calculated, linect is the amount of lines to calculate. Frame increases by one every time the entire image
 //is displayed; this is used to go to the next frame of animation.
-void pretty_effect_calc_lines(uint16_t *dest, int line, int frame, int linect)
-{
+void pretty_effect_calc_lines(uint16_t *dest, int line, int frame, int linect) {
     if (frame!=prev_frame) {
         //We need to calculate a new set of offset coefficients. Take some random sines as offsets to make everything
         //look pretty and fluid-y.
@@ -53,7 +51,6 @@ void pretty_effect_calc_lines(uint16_t *dest, int line, int frame, int linect)
 }
 
 
-esp_err_t pretty_effect_init(void)
-{
+esp_err_t pretty_effect_init(void) {
     return decode_image(&pixels);
 }
--- lcd/main/spi_master_example_main.c
+++ lcd/main/spi_master_example_main.c
@@ -1,26 +1,28 @@
 #include "ILIDriver.h"
 
+const spi_bus_config_t buscfg={
+    .miso_io_num=PIN_NUM_MISO,
+    .mosi_io_num=PIN_NUM_MOSI,
+    .sclk_io_num=PIN_NUM_CLK,
+    .quadwp_io_num=-1,
+    .quadhd_io_num=-1,
+    .max_transfer_sz=PARALLEL_LINES*320*2+8
+};
+
+const spi_device_interface_config_t devcfg={
+#ifdef CONFIG_LCD_OVERCLOCK
+    .clock_speed_hz=40*1000*1000,           //Clock out at 40 MHz
+#else
+    .clock_speed_hz=26*1000*1000,           //Clock out at 26.667 MHz
+#endif
+    .mode=0,                                //SPI mode 0
+    .spics_io_num=PIN_NUM_CS,               //CS pin
+    .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+    .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+};
+
 void app_main(void) {
     spi_device_handle_t spi;
-    spi_bus_config_t buscfg={
-        .miso_io_num=PIN_NUM_MISO,
-        .mosi_io_num=PIN_NUM_MOSI,
-        .sclk_io_num=PIN_NUM_CLK,
-        .quadwp_io_num=-1,
-        .quadhd_io_num=-1,
-        .max_transfer_sz=PARALLEL_LINES*320*2+8
-    };
-    spi_device_interface_config_t devcfg={
-#ifdef CONFIG_LCD_OVERCLOCK
-        .clock_speed_hz=40*1000*1000,           //Clock out at 40 MHz
-#else
-        .clock_speed_hz=26*1000*1000,           //Clock out at 26.667 MHz
-#endif
-        .mode=0,                                //SPI mode 0
-        .spics_io_num=PIN_NUM_CS,               //CS pin
-        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
-        .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
-    };
     //Initialize the SPI bus
     ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
     //Attach the LCD to the SPI bus
