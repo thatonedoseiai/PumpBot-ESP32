bc407dab6d0214f90f83ecd2ac30061dd9f364a5
thatonedoseiai <teslablade@gmail.com>
2023-07-19 17:51:30 -0500

    Adding different fonts for testing, lots of testing broken stuff

--- lcd/main/pb_main.c
+++ lcd/main/pb_main.c
@@ -2,13 +2,13 @@
 #include "decode_image.h"
 #include "wibbly_effect.c"
 #include "freetype2/ft2build.h"
+#include <stdio.h>
+#include <rom/ets_sys.h>
 #include FT_FREETYPE_H
 
 #define SPRITE_LIMIT 16
-
 extern const uint8_t MeiryoUI_ttf_start[] asm("_binary_MeiryoUI_ttf_start");
 extern const uint8_t MeiryoUI_ttf_end[] asm("_binary_MeiryoUI_ttf_end");
-
 const spi_bus_config_t buscfg={
     .miso_io_num=PIN_NUM_MISO,
     .mosi_io_num=PIN_NUM_MOSI,
@@ -30,86 +30,92 @@ const spi_device_interface_config_t devcfg={
     .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
 };
 
-
-// OAM STUFF {{{
-typedef struct oam_handle_ {
-    FT_Bitmap* oam [SPRITE_LIMIT]
-} OAM_HANDLE;
-
-
-OAM_HANDLE* init_OAM() {
-    OAM_HANDLE* ret = (OAM_HANDLE*) malloc(sizeof(OAM_HANDLE));
-    return ret;
-}
-
-void add_char_oam(OAM_HANDLE* oam, FT_Bitmap bmp) {
-    FT_Bitmap* new_bmp = (FT_Bitmap*) malloc(sizeof(FT_Bitmap));
-    /* oam->oam = new_bmp; */
-}
-// }}}
-
-void app_main(void) {
+ void app_main(void) {
+    ets_printf("Reached app_main!\n");
     spi_device_handle_t spi;
+    ets_printf("1\n");
     ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ets_printf("2\n");
     ESP_ERROR_CHECK(spi_bus_add_device(LCD_HOST, &devcfg, &spi));
+    ets_printf("3\n");
     lcd_init(spi);
+    ets_printf("SPI bus initialized!\n");
+    // FT_Library lib;
+    // FT_Face typeFace;
+    // FT_GlyphSlot slot;
+    // FT_Vector offset;
+    // FT_Error error;
+    // char text[] = "嗚呼";
+    // int textLen = 2;
+    // int fontSize = 1;
+    // FT_Long fontBinSize = MeiryoUI_ttf_end - MeiryoUI_ttf_start;
+    // int startX = 0;
+    // int startY = 0;
 
-    FT_Library lib;
-    FT_Face typeFace;
-    FT_GlyphSlot slot;
-    FT_Vector offset;
-    FT_Error error;
-    char text[] = "嗚呼";
-    int textLen = 2;
-    FT_Long fontSize = MeiryoUI_ttf_end - MeiryoUI_ttf_start;
-    int startX = 0;
-    int startY = 0;
+    // uint16_t screenbuf[320*240];
+    // ets_printf("FreeType assignments made!\n");
+    // /*const FT_Open_Args openArgs = {
+    //     .flags = 0x2 | 0x8,
+    //     .memory_base = MeiryoUI_ttf_start,
+    //     .memory_size = fontBinSize,
+    //     //.pathname = unused,
+    //     //.stream = unused,
+    //     .driver = NULL, //TODO: Set to "truetype" and deal with the structs like a big boy
+    //     //.num_params = unused,
+    //     //.params = unused,
+    // };*/
+    //     ets_printf("FreeType Init Starting...\n");
 
-    uint16_t screenbuf[320*240];
+    // error = FT_Init_FreeType(&lib);
+    // if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Init_FreeType! Error:", (int)error);
+    // // error = FT_Open_Face (lib, &openArgs, 0, &typeFace);
+    // // if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Open_Face! Error:", (int)error); 
+    // error = FT_New_Memory_Face(lib, MeiryoUI_ttf_start, fontBinSize, 0, &typeFace);
+    // if(error!=0) ets_printf("%s %d\n", "Error occured @FT_New_Memory_Face! Error:", (int)error);
+    // error = FT_Set_Char_Size (typeFace, fontSize * 64, 0, 100, 0); // 0 = copy last value
+    // if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Set_Char_Size! Error:", (int)error);
 
-    const FT_Open_Args openArgs = {
-        .flags = 0x2 | 0x8,
-        .memory_base = MeiryoUI_ttf_start,
-        .memory_size = fontSize,
-        //.pathname = unused,
-        //.stream = unused,
-        .driver = NULL, //TODO: Set to "truetype" and deal with the structs like a big boy
-        //.num_params = unused,
-        //.params = unused,
-    };
+    // slot = typeFace->glyph;
+    // offset.x = startX * 64;
+    // offset.y = startY * 64;
 
-    error = FT_Init_FreeType(&lib);
-    error = FT_Open_Face (lib, &openArgs, 0, &typeFace);
-    error = FT_Set_Char_Size (typeFace, fontSize * 64, 0, 100, 0); // 0 = copy last value
-    slot = typeFace->glyph;
-    offset.x = startX * 64;
-    offset.y = startY * 64;
+    
+    // for(int n=0;n<textLen;n++) {
+    //     ets_printf("Rendering character...\n");
+    //     FT_Set_Transform(typeFace, NULL, &offset);
+    //     //error = FT_Load_Char(typeFace, text[n], FT_LOAD_RENDER);
+    //     //if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Load_Char! Error:", (int)error);
+    //     //stuff is now in slot -> bitmap
+    //     //add_char_oam(oam, slot->bitmap); 
 
-    for(int n=0;n<textLen;n++) {
-        FT_Set_Transform(typeFace, NULL, &offset);
-        error = FT_Load_Char(typeFace, text[n], FT_LOAD_RENDER);
-        if (error) printf("an error occured when loading the character!");
-        //stuff is now in slot -> bitmap
-        /* add_char_oam(oam, slot->bitmap); */
+    //     //FT_Int bmp_top = 240 - slot->bitmap_top;
 
-        FT_Int bmp_top = 240 - slot->bitmap_top;
+    //     // for(FT_Int p=0,i=slot->bitmap_left; i<slot->bitmap_left+slot->bitmap.width; i++) {
+    //     //     ets_printf("%s\n", "Started for loop 1");
+    //     //     for(FT_Int q=0,j=bmp_top; j<bmp_top+slot->bitmap.rows; j++) {
+    //     //          ets_printf("%s\n", "Started for loop 2");
+    //     //         if(i<0||j<0||i>=320||j>=240) continue;
+    //     //         screenbuf[i+320*j] |= slot->bitmap.buffer[q*slot->bitmap.width+p];
+    //     //         ets_printf("%s\n", "Ended for loop 2");
+    //     //     }
+    //     //     ets_printf("%s\n", "Ended for loop 1");
+    //     // }
 
-        for(FT_Int p=0,i=slot->bitmap_left; i<slot->bitmap_left+slot->bitmap.width; i++) {
-            for(FT_Int q=0,j=bmp_top; j<bmp_top+slot->bitmap.rows; j++) {
-                if(i<0||j<0||i>=320||j>=240) continue;
-                screenbuf[i+320*j] |= slot->bitmap.buffer[q*slot->bitmap.width+p];
-            }
-        }
+    //     offset.x += slot->advance.x;
+    //     offset.y += slot->advance.y;
+    //     ets_printf("Character rendered!\n");
+    // }
 
-        offset.x += slot->advance.x;
-        offset.y += slot->advance.y;
-    }
-
+    ets_printf("Rendering characters done!\n");
+    FT_Done_Face (typeFace);
+    FT_Done_FreeType(lib);
+    ets_printf("Starting to send to display...\n");
     for(int y=0;y<240;y+=PARALLEL_LINES) {
         send_lines(spi, y, screenbuf+320*y);
+        ets_printf("%s %d\n", "sent line", y);
         send_line_finish(spi);
     }
-    printf("%s\n", "finished sending display data!");
+    ets_printf("finished sending display data!\n");
 }
 
 // vim: foldmethod=marker
