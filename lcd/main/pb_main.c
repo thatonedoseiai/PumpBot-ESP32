a91975ffe006ce10c9e31f33a4048d39ec983b36
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-08-11 11:06:05 -0700

    remove unnecessary prints, clean up code with macros

--- lcd/main/pb_main.c
+++ lcd/main/pb_main.c
@@ -5,6 +5,8 @@
 #include FT_FREETYPE_H
 #include "oam.c"
 
+#define FT_ERR_HANDLE(code, loc) if(error) ets_printf("Error occured at %s! Error: %d\n", loc, (int) code);
+
 #define SPRITE_LIMIT 16
 extern const uint8_t MeiryoUI_ttf_start[] asm("_binary_MeiryoUImin_ttf_start");
 extern const uint8_t MeiryoUI_ttf_end[] asm("_binary_MeiryoUImin_ttf_end");
@@ -30,7 +32,7 @@ const spi_device_interface_config_t devcfg={
 };
 
 void app_main(void) {
-	ets_printf("Reached app_main!\n");
+	ets_printf("starting app_main!\n");
 	spi_device_handle_t spi;
 	ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
 	ESP_ERROR_CHECK(spi_bus_add_device(LCD_HOST, &devcfg, &spi));
@@ -62,17 +64,15 @@ void app_main(void) {
 	send_color(spi, fillColor);
 
 	error = FT_Init_FreeType(&lib);
-	if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Init_FreeType! Error:", (int)error);
+	FT_ERR_HANDLE(error, "FT_Init_Freetype");
 	error = FT_New_Memory_Face(lib, MeiryoUI_ttf_start, fontBinSize, 0, &typeFace);
-	if(error!=0) ets_printf("%s %d\n", "Error occured @FT_New_Memory_Face! Error:", (int)error);
+	FT_ERR_HANDLE(error, "FT_New_Memory_Face");
 	error = FT_Select_Charmap(typeFace, FT_ENCODING_UNICODE);
-	if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Select_Charmap! Error:", (int)error);	
+	FT_ERR_HANDLE(error, "FT_Select_Charmap");
 	error = FT_Set_Char_Size (typeFace, fontSize << 6, 0, 100, 0); // 0 = copy last value
-	if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Set_Char_Size! Error:", (int)error);
+	FT_ERR_HANDLE(error, "FT_Set_Char_Size");
 
 	slot = typeFace->glyph;
-	ets_printf("slot = %p\n", slot);
-	//heap_caps_print_heap_info(MALLOC_CAP_8BIT);
 	offset.x = startX << 6;
 	offset.y = startY << 6;
 
@@ -82,33 +82,25 @@ void app_main(void) {
 
 	for(int n=0;n<textLen;n++) {
 		FT_Set_Transform(typeFace, NULL, &offset);
-		ets_printf("Rendering character 0x%x...\n", text[n]);
 		error = FT_Load_Char(typeFace, text[n], FT_LOAD_RENDER | FT_LOAD_TARGET_LCD_V);
-		ets_printf("slot = %p\n", slot);
-		if(error!=0) ets_printf("%s %d\n", "Error occured @FT_Load_Char! Error:", (int)error);
-		//heap_caps_print_heap_info(MALLOC_CAP_8BIT);
+		FT_ERR_HANDLE(error, "FT_Load_Char");
 		uint24_RGB* spriteBuf = (uint24_RGB*) malloc(slot->bitmap.rows * slot->bitmap.width);
-		//heap_caps_print_heap_info(MALLOC_CAP_8BIT);
 		FT_Int bmp_top = 240 - slot->bitmap_top;
 		int sz = slot->bitmap.rows*slot->bitmap.width / 3;
 		for(int p=0;p<sz;p++) {
-			//ets_printf("spriteBuf = %d\n", p);
 			spriteBuf[p].pixelB = slot->bitmap.buffer[p/(slot->bitmap.width)*slot->bitmap.width*3+(p%slot->bitmap.width)];
 			spriteBuf[p].pixelG = slot->bitmap.buffer[p/(slot->bitmap.width)*slot->bitmap.width*3+(p%slot->bitmap.width)+slot->bitmap.width];
 			spriteBuf[p].pixelR = slot->bitmap.buffer[p/(slot->bitmap.width)*slot->bitmap.width*3+(p%slot->bitmap.width)+slot->bitmap.width*2];
 		}
 		init_sprite(spriteBuf, slot->bitmap_left, bmp_top, slot->bitmap.width, slot->bitmap.rows/3, false, false, true);
-		ets_printf("%d x %dpx\n", slot->bitmap.rows / 3, slot->bitmap.width);
-		ets_printf("%d bytes\n", slot->bitmap.rows * slot->bitmap.width);
 
 		offset.x += slot->advance.x;
 		offset.y += slot->advance.y;
 	}
 
-	ets_printf("Rendering characters done!\n");
 	FT_Done_Face (typeFace);
 	FT_Done_FreeType(lib);
-	ets_printf("%s\n", "Sending image data...");
+	ets_printf("%s\n", "Rendering characters done! Sending image data...");
 	draw_all_sprites(spi);
 	ets_printf("finished sending display data!\n");
 
