16038009df14de4eaec833e5c1babb4876db1099
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2024-02-14 18:17:13 -0600

    make different LED channels work

--- lcd/main/mainfs/test.lua
+++ lcd/main/mainfs/test.lua
@@ -1,6 +1,6 @@
 local l = require("lpb")
 
-k=0
+k=1
 l.set_char_size(42<<6)
 spr = {}
 f=0
@@ -10,27 +10,35 @@ back = l.draw_rectangle(71,62,177,46,{16,0,48})
 l.draw_sprites({back})
 oldf = 0
 oldrotenc = true
+channel = {0, 0, 0}
 
 while(l.getgpio(0)) do
     f = l.readrotary()
     if(f[2] ~= oldf) then
         oldf = f[2]
-        if(f[1] == 2) then k = k - 1 else k = k + 1 end
-        if(k >= 0 and k <= 100) then
+        if(f[1] == 2) then channel[k] = channel[k] - 1 else channel[k] = channel[k] + 1 end
+        if(channel[k] >= 0 and channel[k] <= 100) then
             for s=1,#spr do
                 l.delete_sprite(spr[s])
             end
             l.draw_sprites({back})
-            spr = l.draw_text(40, 134, string.format("%.0f%%", k), {255,255,255}, {16,0,48});
+            spr = l.draw_text(40, 134, string.format("%.0f%%", channel[k]), {255,255,255}, {16,0,48});
             l.center_sprites_x(spr)
             l.draw_sprites(spr)
-            l.set_pwm(4, k*163)
+            l.set_pwm(k+3, channel[k]*163)
         else
-            if(k > 100) then
-                k = 100
+            if(channel[k] > 100) then
+                channel[k] = 100
             else
-                k = 0
+                channel[k] = 0
             end
         end
     end
+    if(not l.getgpio(18) and oldrotenc) then
+        k = k + 1
+        if k == 4 then
+            k = 1
+        end
+    end
+    oldrotenc = l.getgpio(18)
 end
