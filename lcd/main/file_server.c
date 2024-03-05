1e347b62a1f266fe58e0a28cae1a1be116f51776
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2024-03-04 23:31:09 -0600

    stop redirecting whenever submission happens

--- lcd/main/file_server.c
+++ lcd/main/file_server.c
@@ -74,6 +74,8 @@ static esp_err_t favicon_get_handler(httpd_req_t *req)
     // const size_t favicon_ico_size = (favicon_ico_end - favicon_ico_start);
     // httpd_resp_set_type(req, "image/x-icon");
     // httpd_resp_send(req, (const char *)favicon_ico_start, favicon_ico_size);
+    char k = 0;
+    httpd_resp_send(req, (const char *)&k, 1);
     return ESP_OK;
 }
 
