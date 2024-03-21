98083dc602b029f0d51d77aca899b13a85273043
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2024-03-20 22:37:13 -0500

    add the wifi symbol functionality (but sudden disconnection may break?)

--- lcd/main/mainfs/test.lua
+++ lcd/main/mainfs/test.lua
@@ -5,6 +5,14 @@ fgcol = l.foreground_color();
 selecting_channels = false
 
 l.set_char_size(12<<6)
+wifi_symbol_status = l.wifi_is_connected();
+w = l.draw_text(306, 226, "w", fgcol, bgcol)
+n = l.draw_text(306, 226, "n", fgcol, bgcol)
+if(wifi_symbol_status) then
+    l.draw_sprites(w)
+else
+    l.draw_sprites(n)
+end
 on_text = l.draw_text(85, 16, "n", fgcol, bgcol)
 off_text = l.draw_text(85, 16, "ff", fgcol, bgcol)
 l.set_char_size(18<<6)
@@ -12,27 +20,23 @@ on_btn_text = l.draw_text(19, 2, "N", fgcol, bgcol)
 off_btn_text = l.draw_text(19, 2, "FF", fgcol, bgcol)
 channels_text = {{},{},{},{}}
 channels_text_bg = {}
+
 for ck=1,4 do
     channels_text[ck] = l.draw_text(0,36,"CH"..(ck-1).."⤓",fgcol,bgcol)
     channels_text_bg[ck] = l.draw_rectangle(129, 240-187-21, 62, 21, bgcol)
     l.center_sprites_x(channels_text[ck])
 end
+f=0
 k=1
 l.set_char_size(42<<6)
 spr = {}
-f=0
 collectgarbage("stop")
 back = l.draw_rectangle(71,240-62-46,177,46,bgcol)
 back_small = l.draw_rectangle(58,240-224-14,51,14,bgcol)
 back_on_off = l.draw_rectangle(89,240-208-16,12,14,bgcol)
 back_btn_text = l.draw_rectangle(21,240-217-21,28,21,bgcol)
 l.draw_sprites({back,back_btn_text})
-oldrotenc = true
-oldleftbtn = true
-leftbtncircbuf = {true, true, true, true, true, true, true, true, true, true}
-leftbtncircbufi = 1
-channel = {0, 0, 0, 0}
--- channel_active = {true, true, true, true}
+-- channel = {0, 0, 0, 0}
 xs = {58,111,164,217}
 l.draw_sprites(off_btn_text)
 l.enable_text_cache_auto_delete(false)
@@ -68,17 +72,6 @@ while(true) do
                 l.draw_sprites({back_on_off, back_btn_text})
                 l.draw_sprites({on_text[1], off_btn_text[1], off_btn_text[2]})
             end
-            -- channel_active[k] = not channel_active[k]
-            -- if(channel_active[k]) then
-            --     l.set_output(k, channel[k]*163)
-            --     l.draw_sprites({back_on_off, back_btn_text})
-            --     l.draw_sprites({on_text[1], off_btn_text[1], off_btn_text[2]})
-            -- else
-            --     l.stop_pwm(k)
-            --     l.draw_sprites({back_on_off, back_btn_text})
-            --     l.draw_sprites({off_text[1], off_text[2], on_btn_text[1]})
-            --     l.draw_sprites({off_text[2]})
-            -- end
         elseif (buttons[1] == 18 and buttons[2] == 1) then
             selecting_channels = not selecting_channels
         end
@@ -86,28 +79,11 @@ while(true) do
 
     if(f ~= nil) then
         if selecting_channels == false then
-            -- if(f[1] == 2) then channel[k] = channel[k] - 1 else channel[k] = channel[k] + 1 end
             if(f[1] == 2) then
                 l.increment_output(k, -163)
             else
                 l.increment_output(k, 163)
             end
-            -- if(channel[k] >= 0 and channel[k] <= 100) then
-            -- update_screen_text(40, 134, {back}, k, true)
-            -- l.sprite_move_x({back_small}, xs[k])
-            -- l.set_char_size(12<<6)
-            -- update_screen_text(xs[k], 2, {back_small}, k, false)
-            -- l.set_char_size(42<<6)
-                -- if(channel_active[k]) then
-                --     l.set_pwm(k, channel[k]*163)
-                -- end
-            -- else
-                -- if(channel[k] > 100) then
-                --     channel[k] = 100
-                -- else
-                --     channel[k] = 0
-                -- end
-            -- end
         else
             if(f[1] == 2) then k = k - 1 else k = k + 1 end
             if k == 5 then
@@ -131,6 +107,14 @@ while(true) do
         l.set_char_size(12<<6)
         update_screen_text(xs[k], 2, {back_small}, k, false)
         l.set_char_size(42<<6)
+    end
+    if(wifi_symbol_status ~= l.wifi_is_connected()) then
+        wifi_symbol_status = l.wifi_is_connected()
+        if(wifi_symbol_status) then
+            l.draw_sprites(w)
+        else
+            l.draw_sprites(n)
+        end
     end
 end
 
