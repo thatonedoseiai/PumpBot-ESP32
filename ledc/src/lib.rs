e764ef364e4a41378df7d8459568d087583cde1a
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2026-07-01 19:21:43 -0500

    temporarily finish LEDC

--- ledc/src/lib.rs
+++ ledc/src/lib.rs
@@ -1,17 +1,33 @@
 //! This crate handles the LED controls. It contains but does not export color-related utilities.
 
-use esp_idf_hal::ledc::*;
-use esp_idf_hal::ledc::config::TimerConfig;
-use esp_idf_hal::prelude::*;
-use esp_idf_hal::gpio::{AnyIOPin};
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+// use esp_idf_hal::ledc::*;
+// use esp_idf_hal::ledc::config::TimerConfig;
+// use esp_idf_hal::prelude::*;
+// use esp_idf_hal::gpio::{AnyIOPin};
+use esp_hal::gpio::{AnyPin, Output, OutputConfig, Level};
+use esp_hal::ledc::{timer::Timer, LowSpeed, channel::{Channel, Number}, Ledc};
+use esp_hal::peripherals::LEDC;
+use esp_hal::delay::Delay;
 use fontfile::RGB;
-use std::sync::{Arc, RwLock};
-use std::thread;
-use std::time::Duration;
+use alloc::sync::Arc;
+use core::time::Duration;
 use embedded_hal::pwm::SetDutyCycle;
+use embassy_executor::Spawner;
+// use embassy_sync::rwlock::RwLock;
+use embassy_sync::once_lock::OnceLock;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use log::info;
 
+// use std::thread;
+
 // --- Types and Configuration ---
+
+type RwLock<T> = embassy_sync::rwlock::RwLock<CriticalSectionRawMutex, T>;
 
 /// Represents an operating mode of the LED cycling engine.
 #[derive(Clone, Debug)]
@@ -39,32 +55,108 @@ pub struct LedController {
 }
 
 /// A collection of all the peripherals needed to run the LED driver.
-pub struct LedPeripherals {
-    led_r: AnyIOPin,
-    led_g: AnyIOPin,
-    led_b: AnyIOPin,
-    channel_r: CHANNEL0,
-    channel_g: CHANNEL1,
-    channel_b: CHANNEL2,
-    timer: TIMER0
+pub struct LedPeripherals<'a> {
+    // led_r: AnyPin,
+    // led_g: AnyPin,
+    // led_b: AnyPin,
+    channel_r: Channel<'a, LowSpeed>,
+    channel_g: Channel<'a, LowSpeed>,
+    channel_b: Channel<'a, LowSpeed>,
+    timer: Timer<'a, LowSpeed>
 }
 
-impl LedPeripherals {
+impl LedPeripherals<'_> {
     /// Instantiates the collection of peripherals needed from the instances provided.
-    pub fn new(
-        led_r: AnyIOPin,
-        led_g: AnyIOPin,
-        led_b: AnyIOPin,
-        channel_r: CHANNEL0,
-        channel_g: CHANNEL1,
-        channel_b: CHANNEL2,
-        timer: TIMER0
-    ) -> Self {
+    pub fn new<'a>(
+        led_r: AnyPin<'a>,
+        led_g: AnyPin<'a>,
+        led_b: AnyPin<'a>,
+        ledc: LEDC<'a>,
+        // channel_r: Channel<'a, LowSpeed>,
+        // channel_g: Channel<'a, LowSpeed>,
+        // channel_b: Channel<'a, LowSpeed>,
+        // timer: Timer<'a, LowSpeed>
+    ) -> LedPeripherals<'a> {
+        let ledc_driver = Ledc::new(ledc);
+        let config = OutputConfig::default();
+        let channel_r = ledc_driver.channel(Number::Channel0, Output::new(led_r, Level::Low, config));
+        let channel_g = ledc_driver.channel(Number::Channel1, Output::new(led_g, Level::Low, config));
+        let channel_b = ledc_driver.channel(Number::Channel2, Output::new(led_b, Level::Low, config));
+        let timer = ledc_driver.timer(esp_hal::ledc::timer::Number::Timer0);
+
         LedPeripherals {
-            led_r, led_g, led_b,
+            // led_r, led_g, led_b,
             channel_r, channel_g, channel_b,
             timer
         }
+    }
+}
+
+static STATE: OnceLock<Arc<RwLock<ControllerState>>> = OnceLock::new();
+
+#[embassy_executor::task]
+async fn ledc_worker(mut p: LedPeripherals<'static>) {
+    // let peripherals = Peripherals::take().unwrap();
+    // let config = TimerConfig::new().resolution(Resolution::Bits14);//.frequency(5.kHz().into());
+    // let timerdriver = LedcTimerDriver::new(p.timer, &config).unwrap();
+
+    // Initialize Channels
+    // let mut ch_r: LedcDriver = LedcDriver::new(p.channel_r, &timerdriver, p.led_r).unwrap();
+    // let mut ch_g: LedcDriver = LedcDriver::new(p.channel_g, &timerdriver, p.led_g).unwrap();
+    // let mut ch_b: LedcDriver = LedcDriver::new(p.channel_b, &timerdriver, p.led_b).unwrap();
+
+    // let max_duty = ch_r.get_max_duty();
+    let max_duty = p.channel_r.max_duty_cycle();
+    let mut tick: u32 = 0;
+    let mut goingup = true;
+    let delay = Delay::new();
+
+    loop {
+        let current = STATE.get().await.clone();
+        
+        let (r, g, b) = match current.read().await.mode {
+            LedMode::Off => (0, 0, 0),
+            LedMode::Solid(color) => (color.r, color.g, color.b),
+            // LedMode::Solid(color) => (255, 0, 0),
+            LedMode::Fade(c1, c2) => {
+                if tick == 0 {
+                    goingup = true
+                }
+                if tick == 255 {
+                    goingup = false;
+                }
+                if goingup {
+                    tick += 1;
+                } else {
+                    tick -= 1;
+                }
+                (
+                    lerp(c1.r, c2.r, tick as u8),
+                    lerp(c1.g, c2.g, tick as u8),
+                    lerp(c1.b, c2.b, tick as u8),
+                )
+            }
+            LedMode::Rainbow => {
+                tick = (tick + 1) % 360;
+                hsv_to_rgb((tick % 360) as f32, 1.0, 1.0)
+            }
+        };
+
+        // Apply brightness and set duty
+        let apply = async |val: u8| -> u16 {
+            (val as u16 * current.read().await.brightness as u16 * max_duty) / 65025
+        };
+
+        // info!("led: ({} {} {}), max: {}", apply(r), apply(g), apply(b), max_duty);
+
+        p.channel_r.set_duty_cycle(apply(r).await).unwrap();
+        p.channel_g.set_duty_cycle(apply(g).await).unwrap();
+        p.channel_b.set_duty_cycle(apply(b).await).unwrap();
+
+        // tick += 2.0; // Increment based on speed
+        // thread::sleep(Duration::from_millis(current.speed_ms));
+        // esp_idf_hal::delay::FreeRtos::delay_ms(current.speed_ms as u32);
+        delay.delay_millis(current.read().await.speed_ms as u32);
     }
 }
 
@@ -73,98 +165,40 @@ impl LedController {
     /// `initial_mode`. This method will also start an independent thread that runs the LEDs
     /// independently.
     pub fn new(
-        p: LedPeripherals,
+        p: LedPeripherals<'static>,
         initial_mode: LedMode,
+        spawner: Spawner,
     ) -> Self {
-        let state = Arc::new(RwLock::new(ControllerState {
+        let inner_arc = Arc::new(RwLock::new(ControllerState {
             mode: initial_mode,
             brightness: 255,
             speed_ms: 10,
         }));
-
-        let thread_state = state.clone();
+        STATE.init(inner_arc.clone()).unwrap();
 
         // Spawn the worker thread
-        thread::spawn(move || {
-            // let peripherals = Peripherals::take().unwrap();
-            let config = TimerConfig::new().resolution(Resolution::Bits14);//.frequency(5.kHz().into());
-            let timerdriver = LedcTimerDriver::new(p.timer, &config).unwrap();
+        // thread::spawn(move || { });
+        let _ = spawner.spawn(ledc_worker(p).unwrap());
 
-            // Initialize Channels
-            let mut ch_r: LedcDriver = LedcDriver::new(p.channel_r, &timerdriver, p.led_r).unwrap();
-            let mut ch_g: LedcDriver = LedcDriver::new(p.channel_g, &timerdriver, p.led_g).unwrap();
-            let mut ch_b: LedcDriver = LedcDriver::new(p.channel_b, &timerdriver, p.led_b).unwrap();
-
-            let max_duty = ch_r.get_max_duty();
-            let mut tick: u32 = 0;
-            let mut goingup = true;
-
-            loop {
-                let current = { thread_state.read().unwrap().clone() };
-                
-                let (r, g, b) = match current.mode {
-                    LedMode::Off => (0, 0, 0),
-                    LedMode::Solid(color) => (color.r, color.g, color.b),
-                    // LedMode::Solid(color) => (255, 0, 0),
-                    LedMode::Fade(c1, c2) => {
-                        if tick == 0 {
-                            goingup = true
-                        }
-                        if tick == 255 {
-                            goingup = false;
-                        }
-                        if goingup {
-                            tick += 1;
-                        } else {
-                            tick -= 1;
-                        }
-                        (
-                            lerp(c1.r, c2.r, tick as u8),
-                            lerp(c1.g, c2.g, tick as u8),
-                            lerp(c1.b, c2.b, tick as u8),
-                        )
-                    }
-                    LedMode::Rainbow => {
-                        tick = (tick + 1) % 360;
-                        hsv_to_rgb((tick % 360) as f32, 1.0, 1.0)
-                    }
-                };
-
-                // Apply brightness and set duty
-                let apply = |val: u8| -> u32 {
-                    (val as u32 * current.brightness as u32 * max_duty) / 65025
-                };
-
-                // info!("led: ({} {} {}), max: {}", apply(r), apply(g), apply(b), max_duty);
-
-                ch_r.set_duty(apply(r)).unwrap();
-                ch_g.set_duty(apply(g)).unwrap();
-                ch_b.set_duty(apply(b)).unwrap();
-
-                // tick += 2.0; // Increment based on speed
-                // thread::sleep(Duration::from_millis(current.speed_ms));
-                esp_idf_hal::delay::FreeRtos::delay_ms(current.speed_ms as u32);
-            }
-        });
-
-        Self { state }
+        Self { state: inner_arc }
     }
 
+    // TODO: add the errors or make it async
     /// Sets the current operating mode of the LED driver to `mode`.
     pub fn update_mode(&self, mode: LedMode) {
-        let mut s = self.state.write().unwrap();
+        let mut s = self.state.try_write().unwrap();
         s.mode = mode;
     }
 
     /// Sets the current brightness of the LEDs to `brightness`. 0 < `brightness` < 255
     pub fn set_brightness(&self, brightness: u8) {
-        let mut s = self.state.write().unwrap();
+        let mut s = self.state.try_write().unwrap();
         s.brightness = brightness;
     }
 
     /// Sets the period between updates to `speed_ms` milliseconds.
     pub fn set_speed(&self, speed_ms: u64) {
-        let mut s = self.state.write().unwrap();
+        let mut s = self.state.try_write().unwrap();
         s.speed_ms = speed_ms;
     }
 }
